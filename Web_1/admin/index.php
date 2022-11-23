e7d7fd15f8586ba2b781a8c9d49359fb984ed6cf
Achini Thathsarani <achinit720@gmail.com>
2022-11-23 22:12:18 +0530

    Add files via upload

---
+++ Web_1/admin/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- Menu Section Starts -->
+    <div class="menu">
+        <div class="wrapper">
+            Menu Goes Here
+        </div>
+    </div>
+    <!-- Menu Section Ends -->
+
+    <!-- Main Content Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            Main Content Goes Here
+        </div>
+    </div>
+    <!-- Main Content Section Ends -->
+
+    <!-- Footer Section Starts -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class=text-center>Lorem ipsum, dolor sit amet consectetur adipisicing elit - <a href="#">Odio quae nulla omnis</a></p>
+        </div>
+    </div>
+    <!-- Footer Section Ends -->
+
+</body>
+</html>
